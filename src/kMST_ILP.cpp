29467096fee60e16f4af584771616cb2b8d074ed
Dino Rossegger <e0926471@student.tuwien.ac.at>
2013-12-30 19:09:50 +0100

    ad

--- src/kMST_ILP.cpp
+++ src/kMST_ILP.cpp
@@ -39,6 +39,7 @@ void kMST_ILP::solve()
 		cout << "Branch-and-Bound nodes: " << cplex.getNnodes() << "\n";
 		cout << "Objective value: " << cplex.getObjValue() << "\n";
 		cout << "CPU time: " << Tools::CPUtime() << "\n\n";
+		cout << model;
 	}
 	catch( IloException& e ) {
 		cerr << "kMST_ILP: exception " << e << "\n";
@@ -78,16 +79,23 @@ void kMST_ILP::modelMCF()
 void kMST_ILP::modelMTZ()
 {
 	//x
-	IloBoolVarArray x(env,m);
+	IloBoolVarArray x(env,2*m);
 	for(int i=0;i<m;i++){
 		stringstream myname;
 		myname << "x_" << instance.edges.at(i).v1 << "," <<instance.edges.at(i).v2;
 		x[i]=IloBoolVar(env,myname.str().c_str());
+		
+		stringstream myname2;
+		myname2 << "x_" << instance.edges.at(i).v2 << "," << instance.edges.at(i).v1;
+		x[i+m]=IloBoolVar(env,myname2.str().c_str());
+		
 	}
 	//c
-	int c[m];
+	int c[2*m];
 	for(int i=0;i<m;i++){
 		c[i]=instance.edges.at(i).weight;
+
+		c[i+m]=instance.edges.at(i).weight;
 	}
 	//u
 	IloIntVarArray u(env, n);
@@ -109,79 +117,85 @@ void kMST_ILP::modelMTZ()
 	IloExpr objfunc(env);
 	for (int i=0;i<m; i++){
 		objfunc +=x[i]*c[i];
+		objfunc +=c[i+m]*x[i+m];
 	}
 	model.add(IloMinimize(env,objfunc));
 	objfunc.end();
 	
 	//(2)
 	for (int i=0;i<m;i++){
-		IloExpr co3_0(env);
-		co3_0+=k*x[i];
-		co3_0+=u[instance.edges.at(i).v1];
+		model.add(x[i]+u[instance.edges.at(i).v1] <= u[instance.edges.at(i).v2] + k*(1-x[i]));
 
-		IloExpr co3_1(env);
-		co3_1+=u[instance.edges.at(i).v2];
-		co3_1+=k-1;
-		model.add(co3_0 <= co3_1);
-		co3_0.end();
-		co3_1.end();
+		model.add(x[i+m]+u[instance.edges.at(i).v2] <= u[instance.edges.at(i).v1] + k*(1-x[i+m]));
 	}
 	//(3)
-	IloExpr co3(env);
-	co3=u[0];
-	model.add(co3==0);
-	co3.end();
-
-	//(4)
-	IloExpr co4(env);
-	for (int i=0;i<m;i++){
-		if(instance.edges.at(i).v1==0 or instance.edges.at(i).v2==0){
-			co4+=x[i];
+	model.add(u[0]==0);
+//(4) 
+	IloExpr co4(env); 
+	for (int i=0;i<m;i++){ 
+		if(instance.edges.at(i).v1==0 /*or instance.edges.at(i).v2==0*/){ 
+			co4+=x[i]; 
+		}
+		if(instance.edges.at(i).v2==0){
+			co4+=x[i+m];
 		}
 	}
 	model.add(co4 == 1);
 	co4.end();
 	//(5)	
-	for (int i=0;i<n;i++){
+	for (int i=1;i<n;i++){
 		model.add(0<=u[i]);
 		model.add(u[i]<=k);
 	}
 
 	//(6)
 	IloExpr co6(env);
-	for (int i=0;i<m;i++){
+	for (int i=0;i<2*m;i++){
 				co6+=x[i];
 	}
 	model.add(co6 == k);
 	co6.end();
 
 	//(7)
-	for(int i=0;i<n;i++){
+	for(int i=1;i<n;i++){
 		IloExpr co7(env);
-		for(int j=0;j<m;j++){
-			if(instance.edges.at(j).v2==i)
-				co7+=x[j];
+		for(list<u_int>::iterator it=instance.incidentEdges.at(i).begin();it != instance.incidentEdges.at(i).end();++it){
+			if(instance.edges.at(*it).v2==i)
+				co7+=x[*it];
+			else
+				co7+=x[*it+m];
 		}
 		model.add(co7 <= 1);
 		co7.end();
 	}
-	//(8)
+	/*//(8)
 	IloExpr co8(env);
-	for(int i=1;i<n;i++){
+	for(int i=0;i<n;i++){
 		co8+=v[i];
 	}
 	model.add(co8==k);
-	co8.end();
+	co8.end();*/
 	//(9)
-	for(int i=1;i<n;i++){
-		model.add(u[i]<=k*v[i]);
+	for(int i=0;i<n;i++){
+		IloExpr co9(env);
+		co9=u[i];
+		model.add(co9<=k*v[i]);
+		co9.end();
 	}
 	//(10)-(11)
 	for(int i=0;i<m;i++){
-		if(instance.edges.at(i).v1!=0)
-		model.add(x[i]<=u[instance.edges.at(i).v1]);
-		if(instance.edges.at(i).v2!=0)
-		model.add(x[i]<=u[instance.edges.at(i).v2]);
+			if(instance.edges.at(i).v1!=0 and instance.edges.at(i).v2!=0){
+				IloExpr co10(env),co11(env),co11_1(env),co10_1(env);
+				co10=x[i];
+				co11=x[m+i];
+				co10_1=u[instance.edges.at(i).v1];
+				co11_1=u[instance.edges.at(i).v2];
+				model.add(co10<=co10_1);
+				model.add(co11<=co10_1);
+				model.add(co10<=co11_1);
+				model.add(co11<=co11_1);
+				co10.end();co11.end();co11_1.end();co10_1.end();
+			}
 	}
 	//(12)
 	IloExpr co12(env);
